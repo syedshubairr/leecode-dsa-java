53602dd304c754afac1b8f79ec0e119f6a083719
Shubair Raza <110731556+syedshubairr@users.noreply.github.com>
2025-05-31 22:43:42 +0500

    Greatest between 3 numbers

--- src/Loops/LargestNumber.java
+++ src/Loops/LargestNumber.java
@@ -1,4 +1,31 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int max = 0;
+        if (a >= b && a >= c) {
+            max = a;
+        } else if (b >= a && b >= c) {
+            max = b;
+        } else {
+            max = c;
+        }
+        System.out.println(max);
+        // Another approach.
+        max = 0;
+        max = a;
+        if (max <= b) {
+            max = b;
+        }
+        if (max <= c) {
+            max = c;
+        }
+        System.out.println(max);
+    }
 }
