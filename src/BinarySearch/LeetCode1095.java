b36bab4b2f2504e4ba489574fd1bf1e20c8e0f3b
Shubair Raza <110731556+syedshubairr@users.noreply.github.com>
2025-06-20 00:10:15 +0500

    leet code 1095 completed.

--- src/BinarySearch/LeetCode1095.java
+++ src/BinarySearch/LeetCode1095.java
@@ -5,30 +5,42 @@ public class LeetCode1095 {
 //        int[] arr = {1, 2, 3, 4, 5, 3, 1};
         int[] arr = {3, 5, 3, 2, 0};
         int target = 5;
-        System.out.println("Ans: " + findInMountainArray(target, arr));
-    }
-
-    public static int findInMountainArray(int target, int[] mountainArr) {
-        int ans = findElement(target, mountainArr, true);
+        int peek = peekMountain(arr);
+        int ans = findElement(target, arr, 0, peek);
         if (ans == -1) {
-            return findElement(target, mountainArr, false);
+            ans = findElement(target, arr, peek + 1, arr.length - 1);
         }
-        return ans;
+        System.out.println("Found" + ans);
     }
 
-    public static int findElement(int target, int[] a, boolean inStart) {
+    public static int peekMountain(int[] arr) {
         int start = 0;
-        int end = a.length - 1;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                // we are in the decreasing part of the array.
+                // we may have found the answer, but we are not sure if that the correct answer.
+                end = mid;
+            } else {
+                // we are in the increasing part of the array.
+                // we know that mid > mid + 1, so thats why we are incrementing the array but one.
+                start = mid + 1;
+            }
+        }
+        // returning start, as our start element will be on the peek.
+        return start;
+    }
+
+    public static int findElement(int target, int[] a, int start, int end) {
         while (start <= end) {
             int mid = start + (end - start) / 2;
             if (target < a[mid]) {
                 end = mid - 1;
+            } else if (target > a[mid]) {
+                start = start + 1;
             } else {
-                if (inStart) {
-                    end = mid - 1;
-                } else {
-                    start = mid;
-                }
+                return mid;
             }
         }
         return -1;
