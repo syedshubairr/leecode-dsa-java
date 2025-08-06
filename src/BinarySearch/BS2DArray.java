e31db2215d143445e161a62f03eda2ac648e5ddd
Shubair Raza <110731556+syedshubairr@users.noreply.github.com>
2025-08-06 20:55:42 +0500

    Binary Search in 2D Array done.

--- src/BinarySearch/BS2DArray.java
+++ src/BinarySearch/BS2DArray.java
@@ -10,19 +10,32 @@ public class BS2DArray {
                 {28, 29, 37, 49},
                 {33, 34, 38, 50}
         };
+        int[][] arr2 = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
         int target = 37;
+        int target2 = 14;
         int[] found = findIn2d(arr, target);
         if (found == null) {
             System.out.println("Not Found");
             return;
         }
-        System.out.println("Found at: " + Arrays.toString(found));
+        System.out.println("Row-Col Sorted Found at: " + Arrays.toString(found));
+        found = findInStrictlySortedArray(arr2, target2);
+        if (found == null) {
+            System.out.println("Not Found");
+            return;
+        }
+        System.out.println("Strictly Sorted Found at: " + Arrays.toString(found));
     }
 
     public static int[] findIn2d(int[][] arr, int target) {
         int row = 0;
         int column = arr[0].length - 1;
-        while (row < arr[0].length - 1 && column > 0) {
+        while (row < arr[0].length && column >= 0) {
             if (target == arr[row][column]) {
                 System.out.println("The Element is found");
                 return new int[]{row, column};
@@ -30,6 +43,22 @@ public class BS2DArray {
                 row++;
             } else if (target < arr[row][column]) {
                 column--;
+            }
+        }
+        return null;
+    }
+
+    public static int[] findInStrictlySortedArray(int[][] arr, int target) {
+        int row = 0;
+        int col = arr.length - 1;
+        while (row < arr[0].length && col >= 0) {
+            if (target == arr[row][col]) {
+                System.out.println("The Element is found");
+                return new int[]{row, col};
+            } else if (target > arr[row][col]) {
+                row++;
+            } else if (target < arr[row][col]) {
+                col--;
             }
         }
         return null;
